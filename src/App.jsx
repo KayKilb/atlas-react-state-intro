c5434185bb6533a4309ea2fb6a7341d9bd7bba12
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-09-04 12:13:44 -0500

    added import for CourseProvider in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import SchoolCatalog from "./SchoolCatalog"; // Import the SchoolCatalog component
 import Header from "./Header"; // Import the Header component
 import ClassSchedule from "./ClassSchedule"; // Import the ClassSchedule component
+import { CourseProvider} "./CourseContext"; // Import the CourseProvider componentontext"; // Import the CourseProvider component  
 
 export default function App() {
   return (
-    <div>
+    <CourseProvider>
       <Header /> {/* Render the Header component */}
       <SchoolCatalog /> {/* Render the SchoolCatalog component */}
       <ClassSchedule /> {/* Render the ClassSchedule component */}
-    </div>
+    </CourseProvider>
   );
 }
