a21e525c4540cf28eacd50fa5429b539d2403c5e
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-09-04 11:55:29 -0500

    created CourseContext.jsx file

---
+++ src/CourseContext.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useContext } from 'react';
+
+// Create context for course enrollment
+const CourseContext = createContext();
+
+export function useCourseContext(); {
+    return useContext(CourseContext);
+}
+
+export function CourseProvider({ children }) {
+    const [enrolledCourses, setEnrolledCourses] = useState([]);
+
+    const enrollCourse = (course) => {
+        setEnrolledCourses(prevCourses) => [...prevCourses, course]);
+    };
+    
+    const dropCourse = (courseNumber) => {
+        setEnrolledCourses((prevCourses) =>
+            prevCourses.filter((course) => course.courseNumber !== courseNumber)
+    };
+};
