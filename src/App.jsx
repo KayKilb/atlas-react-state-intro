8a6ef9a3b3a9156b2b1283517fb5481f0a4d2849
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-09-04 12:24:16 -0500

    removed and pasted again the code for App, not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import SchoolCatalog from "./SchoolCatalog"; // Import the SchoolCatalog component
-import Header from "./Header"; // Import the Header component
-import ClassSchedule from "./ClassSchedule"; // Import the ClassSchedule component
-import { CourseProvider } "./CourseContext"; // Import the CourseProvider componentontext"; // Import the CourseProvider component  
+import React from "react";
+import SchoolCatalog from "./SchoolCatalog";
+import Header from "./Header";
+import ClassSchedule from "./ClassSchedule";
+import { CourseProvider } from "./CourseContext"; // Import the CourseProvider
 
 export default function App() {
   return (
     <CourseProvider>
-      <Header /> {/* Render the Header component */}
-      <SchoolCatalog /> {/* Render the SchoolCatalog component */}
-      <ClassSchedule /> {/* Render the ClassSchedule component */}
+      <Header />
+      <SchoolCatalog />
+      <ClassSchedule />
     </CourseProvider>
   );
 }
